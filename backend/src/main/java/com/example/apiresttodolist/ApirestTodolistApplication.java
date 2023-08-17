05d98c5f1d65ffd4402195ad03536437f8adca49
carmenb2020 <you@example.com>
2023-08-17 11:16:36 +0200

    first commit

---
+++ backend/src/main/java/com/example/apiresttodolist/ApirestTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiresttodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestTodolistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/apiresttodolist/ApirestTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiresttodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
