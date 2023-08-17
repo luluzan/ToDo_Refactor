539c1ed97f82c38c640b402fe087ebd779f5568a
LeoloFlrz <leoloflrzr@gmail.com>
2023-08-17 15:06:06 +0200

    added methods to controller

--- backend/src/main/java/com/todolist/controller/ToDosController.java
+++ backend/src/main/java/com/todolist/controller/ToDosController.java
@@ -1,4 +1,36 @@
 package com.todolist.controller;
 
-public class ToDosController {
+import com.todolist.model.ToDo;
+import com.todolist.services.ToDosServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping(path = "/todo")
+public class ToDosController{
+    @Autowired
+    private ToDosServices toDosServices;
+
+    @GetMapping(path = "/list")
+    public ArrayList<ToDo> getAllTasks() {
+        return toDosServices.getAllTasks();
+    }
+
+    @GetMapping(path = "/list/{id}")
+    public ToDo getTaskById(@PathVariable long id) {
+        return toDosServices.getTaskById(id);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public String deleteTask(@PathVariable long id) {
+        return toDosServices.deleteTask(id);
+    }
+
+    @PostMapping(path = "/add")
+    public String saveTask(@RequestBody ToDo toDo) {
+        return (toDosServices.saveTask(toDo));
+    }
 }
