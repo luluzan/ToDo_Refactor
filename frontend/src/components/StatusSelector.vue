664a893cc2f4166d79256aa48dc2eed270a7e9bf
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-08-23 08:35:44 +0200

    added StatusSelector to dev

---
+++ frontend/src/components/StatusSelector.vue
@@ -0,0 +1,27 @@
+<script setup>
+import GetTasks from "../services/ApiConnection"
+
+	const	props = defineProps (
+		{
+			id:
+			{
+				type: Number,
+			}
+		}
+	)
+
+	const	getTasks = new GetTasks();	
+	const	ftStatusChange = async() =>
+	{
+		const	task = await getTasks.getTaskById(props.id);
+
+		//console.log(task.data.status);
+		task.data.status = !task.data.status;
+		await getTasks.updateTask(props.id, task.data);
+	}
+
+</script>
+
+<template>
+	<input type="checkbox" @change="ftStatusChange()">
+</template>
