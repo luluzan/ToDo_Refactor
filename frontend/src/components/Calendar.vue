b4a5e49379207e57d626ecd2751005af762a94f1
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-08-25 13:12:52 +0200

    little change to date format

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -14,6 +14,6 @@ onUpdated(() => {
 </script>
 
 <template>
-  <VueDatePicker v-model="date" modelType="dd.MM.yyyy"></VueDatePicker>
+  <VueDatePicker v-model="date" modelType="yyyy.MM.dd"></VueDatePicker>
 </template>
 
