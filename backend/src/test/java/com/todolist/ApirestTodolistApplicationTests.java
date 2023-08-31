86d21bce8b531186422449a572f3d49b56939f0e
Dani <dani.crespo.rodriguez@gmail.com>
2023-08-31 15:24:46 +0200

    WIP Backen tests

--- backend/src/test/java/com/todolist/ApirestTodolistApplicationTests.java
+++ backend/src/test/java/com/todolist/ApirestTodolistApplicationTests.java
@@ -1,13 +1,29 @@
 package com.todolist;
 
+import com.todolist.model.ToDo;
+import com.todolist.repository.ToDosRepository;
+import com.todolist.services.ToDosServices;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ApirestTodolistApplicationTests {
 
+	@InjectMocks
+	ToDosServices myServices;
+
+	@Mock
+	ToDosRepository myRepository;
+
 	@Test
-	void contextLoads() {
+	void test_should_get_all_tasks() {
+		//Entorno
+		ToDo task1= new ToDo(1L,"Hacer test de back", "Implementar mockito",);
 	}
 
 }
